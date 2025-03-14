7c281ca3cb7dec12364ddc2dc6a6a82dee1212f8
Евтюхин Дмитрий Валерьевич <d.evtyukhin@abstechnology.ru>
2025-03-14 10:02:50 +0300

    Initial commit

---
+++ src/main/java/ru/absolutins/camundaFirstApp/CamundaFirstAppApplication.java
@@ -0,0 +1,13 @@
+package ru.absolutins.camundaFirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaFirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/absolutins/camundaFirstApp/CamundaFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.absolutins.camundaFirstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
